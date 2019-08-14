40401bd941011fd34eee6c5fdb3c3ae09c5624db
yjc <1972077620@qq.com>
2019-08-14 14:32:36 +0800

    mydev修改了项目，新增Mayun.java

---
+++ src/gitTest/Mayun.java
@@ -0,0 +1,19 @@
+package gitTest;
+
+public class Mayun {
+	private String myName;
+	private String myPwd;
+	public String getMyName() {
+		return myName;
+	}
+	public void setMyName(String myName) {
+		this.myName = myName;
+	}
+	public String getMyPwd() {
+		return myPwd;
+	}
+	public void setMyPwd(String myPwd) {
+		this.myPwd = myPwd;
+	}
+	
+}
